b986e3e2e2a2c7cbd7357c434acb642e8ff32b84
GabrieleMaurina <gabrielemaurina95@gmail.com>
2024-07-23 12:00:25 -0600

    Add temperature. (#2)
    * Added temperature.
    
    * Added temperature.

--- weather/__init__.py
+++ weather/__init__.py
@@ -1,13 +1,32 @@
 import flet
 
+import weather.forecast
 import weather.location
 
 
 def weather_app(page: flet.Page) -> None:
     lat, lon, address = weather.location.get_lat_lon()
+    curr_temp, min_temp, max_temp = weather.forecast.get_forecast(lat, lon)
     page.title = "Weather"
     page.vertical_alignment = flet.MainAxisAlignment.CENTER
     page.add(
         flet.Row([flet.Text(address, size=50)], alignment=flet.MainAxisAlignment.CENTER)
     )
-    lat, lon, address = weather.location.get_lat_lon()
+    page.add(
+        flet.Row(
+            [flet.Text(f"Temperature: {curr_temp}°F", size=50)],
+            alignment=flet.MainAxisAlignment.CENTER,
+        )
+    )
+    page.add(
+        flet.Row(
+            [flet.Text(f"Min: {min_temp}°F", size=30)],
+            alignment=flet.MainAxisAlignment.CENTER,
+        )
+    )
+    page.add(
+        flet.Row(
+            [flet.Text(f"Max: {max_temp}°F", size=30)],
+            alignment=flet.MainAxisAlignment.CENTER,
+        )
+    )
