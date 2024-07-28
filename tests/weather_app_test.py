de9d8b5b08564b8d56efd5f9faf47d0c6f2d1476
GabrieleMaurina <gabrielemaurina95@gmail.com>
2024-07-27 18:48:40 -0600

    Changed title.

--- tests/weather_app_test.py
+++ tests/weather_app_test.py
@@ -7,6 +7,6 @@ import weather_desktop
 def test_weather_app(mocker: pytest_mock.MockFixture) -> None:
     page = mocker.Mock(spec=flet.Page)
     weather_desktop.weather_app(page)
-    assert page.title == "Weather"
+    assert page.title == "Weather Desktop"
     assert page.vertical_alignment == flet.MainAxisAlignment.CENTER
     assert page.add.call_args[0][0].controls[0].value
--- weather_desktop/__init__.py
+++ weather_desktop/__init__.py
@@ -7,7 +7,7 @@ import weather_desktop.location
 def weather_app(page: flet.Page) -> None:
     lat, lon, address = weather_desktop.location.get_lat_lon()
     curr_temp, min_temp, max_temp = weather_desktop.forecast.get_forecast(lat, lon)
-    page.title = "Weather"
+    page.title = "Weather Desktop"
     page.vertical_alignment = flet.MainAxisAlignment.CENTER
     page.add(
         flet.Row([flet.Text(address, size=50)], alignment=flet.MainAxisAlignment.CENTER)
